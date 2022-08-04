f7756bbd3d1a18060c5a72e4561a71510c7d87c2
Dmitrii Gilep <dmitry.gilep@gmail.com>
2022-08-04 19:34:14 +0300

    установил значения по умолчанию

---
+++ Industrial/Industrial/Profile/CurrentUserService.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentUserService.swift
+//  Industrial
+//
+//  Created by DmitriiG on 04.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class CurrentUserService: UserService {
+    
+    var user = User(name: "Dmitrii", avatar: UIImage(named: "Avatar.jpeg")!, status: "Thinking")
+    
+    func userInfo(name: String) -> User? {
+        if name == user.name  {return user}
+        else {return User(name: "NoName", avatar: UIImage(named: "Avatar.jpeg")!, status: "Not logged in")}
+    }
+}
