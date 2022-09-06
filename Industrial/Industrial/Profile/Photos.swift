bbf6deeee08cac42d0e7064861e597306fd324b9
Dmitrii Gilep <dmitry.gilep@gmail.com>
2022-09-06 21:11:55 +0300

    перенес фильтр в controller, добавил замыкание для запуска reloadData по факту обновления массива после применения фильтра, замер времени изменил: начало в начале функции, конец в конце completion внутри, так как completion является escaping и заканчивает работу позднее не в главном потоке

--- Industrial/Industrial/Profile/Photos.swift
+++ Industrial/Industrial/Profile/Photos.swift
@@ -14,51 +14,32 @@ import iOSIntPackage
 //}
 
 final class PhotoData {
-    
-    let imageProcessor = ImageProcessor()
-
-    var imagesArray: [UIImage] = []
         
-    init(photo:[Int]) {
-        createPhotoDataInt(photo: photo)
-        applyFilterToImagesArrayWithTimer()
+    var imageArrayUpdateStatus: (() -> Void)?
+    
+    var imagesArray: [UIImage] = [] {
+        didSet {
+            self.imageArrayUpdateStatus?()
+        }
     }
-
+    
+    init(photos:[Int]) {
+        createPhotoDataInt(photos: photos)
+    }
+    
     // изменил методы - один для добавления массива картинок, полученных от observer, другой для добавления по имени из xcasset
-//    func createPhotoDataUIImage(photo: [UIImage]) {
-//        photo.forEach {
-//            imagesArray.append($0)
-//        }
-//    }
+    //    func createPhotoDataUIImage(photo: [UIImage]) {
+    //        photo.forEach {
+    //            imagesArray.append($0)
+    //        }
+    //    }
     
-    func createPhotoDataInt(photo: [Int]) {
-        photo.forEach {
-            guard let image = UIImage(named: "\(String($0))") else { return }
-            imagesArray.append(image)
-        }
-    }
-    
-    // включил метод по применению фильтра с completion по преобразованию массива в массив UIImage и присвоению его значения массиву imagesArray
-    func applyFilterToImagesArrayWithTimer() {
-    //замер времени на функцию
-        let start = DispatchTime.now() // start time
-    imageProcessor.processImagesOnThread(
-        sourceImages: self.imagesArray,
-        filter: .colorInvert,
-        qos: .default) {
-            outPutImages in
-            self.imagesArray = outPutImages.map { cgImage -> UIImage in
-                guard let image = cgImage else { return UIImage(named: "1")!
+            func createPhotoDataInt(photos: [Int]) {
+                photos.forEach {
+                    guard let image = UIImage(named: "\(String($0))") else { return }
+                    imagesArray.append(image)
                 }
-                let filteredImage = UIImage(cgImage: image)
-                return filteredImage
             }
-        }
-    let end = DispatchTime.now()   // end time
-    let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
-    let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
-    print("!!!TIME FOR processImagesOnThread = \(timeInterval)")
-    }
     
 }
 
