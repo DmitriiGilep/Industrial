4a61d954d734f93bed5da6cfd7a95574d3733af5
Dmitrii Gilep <dmitry.gilep@gmail.com>
2022-10-28 12:12:22 +0300

    update

--- Industrial/Industrial/Profile/CheckerService.swift
+++ Industrial/Industrial/Profile/CheckerService.swift
@@ -15,18 +15,18 @@ final class CheckerService: CheckerServiceProtocol {
     func signUp(login: String, password: String, controller: LogInViewController, coordinator: ProfileCoordinator) {
         Auth.auth().createUser(withEmail: login, password: password) { [weak self] authResult, error in
             if error == nil {
-//                let alertView = self!.createAlertView(viewTitle: "Успешная регистрация", message: "Пользователь успешно зарегистрирован", actionTitle: "ok")
-                let alertView = UIAlertController(title: "Успешная регистрация", message: "Пользователь успешно зарегистрирован", preferredStyle: .alert)
-                let ok = UIAlertAction(title: "ok", style: .default)
-                alertView.addAction(ok)
+                let alertView = self!.createAlertView(viewTitle: "Успешная регистрация", message: "Пользователь успешно зарегистрирован", actionTitle: "ok")
+//                let alertView = UIAlertController(title: "Успешная регистрация", message: "Пользователь успешно зарегистрирован", preferredStyle: .alert)
+//                let ok = UIAlertAction(title: "ok", style: .default)
+//                alertView.addAction(ok)
                 controller.present(alertView, animated: true, completion: nil)
             } else {
 
-//                let alertView = self!.createAlertView(viewTitle: "Ошибка регистрации", message: error!.localizedDescription, actionTitle: "ok")
+                let alertView = self!.createAlertView(viewTitle: "Ошибка регистрации", message: error!.localizedDescription, actionTitle: "ok")
                 
-                let alertView = UIAlertController(title: "Ошибка регистрации", message: error!.localizedDescription, preferredStyle: .alert)
-                let ok = UIAlertAction(title: "ok", style: .default)
-                alertView.addAction(ok)
+//                let alertView = UIAlertController(title: "Ошибка регистрации", message: error!.localizedDescription, preferredStyle: .alert)
+//                let ok = UIAlertAction(title: "ok", style: .default)
+//                alertView.addAction(ok)
                 controller.present(alertView, animated: true, completion: nil)
             }
         }
@@ -63,11 +63,11 @@ final class CheckerService: CheckerServiceProtocol {
         }
     }
     
-//    private func createAlertView(viewTitle: String, message: String, actionTitle: String) -> UIAlertController {
-//        let alertView = UIAlertController(title: viewTitle, message: message, preferredStyle: .alert)
-//        let action = UIAlertAction(title: actionTitle, style: .default)
-//        alertView.addAction(action)
-//        return alertView
-//    }
+    private func createAlertView(viewTitle: String, message: String, actionTitle: String) -> UIAlertController {
+        let alertView = UIAlertController(title: viewTitle, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .default)
+        alertView.addAction(action)
+        return alertView
+    }
     
 }
--- Industrial/Industrial/Profile/LoginInspector.swift
+++ Industrial/Industrial/Profile/LoginInspector.swift
@@ -11,13 +11,13 @@ import UIKit
 
 final class LoginInspector: LoginViewControllerDelegate {
     
+    let checkerService = CheckerService() // переменная должна быть вне completion, то есть нужно держать ссылку на CheckerService, если создаешь внутри, то ссылка не держится, когда асинхронно вызывается комплишн, этого сервиса уже нет, его никто не держит.
+
     func checkCredentials(login: String, password: String, controller: LogInViewController, coordinator: ProfileCoordinator) {
-        let checkerService = CheckerService()
         checkerService.checkCredentials(login: login, password: password, controller: controller, coordinator: coordinator)
     }
     
     func signUp(login: String, password: String, controller: LogInViewController, coordinator: ProfileCoordinator) {
-        let checkerService = CheckerService()
         checkerService.signUp(login: login, password: password, controller: controller, coordinator: coordinator)
     }
     
